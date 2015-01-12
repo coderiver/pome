da70dc1eee136516aa4baedd50a8025bdd9e725c
andreyka_g <andreyka_g@ukr.net>
2015-01-12 18:06:54 +0200

    html added

--- js/common.js
+++ js/common.js
@@ -14,6 +14,5 @@ head.ready(function() {
 	// $(window).scroll(function(){
 	//     scrollFixedElements()
 	// });
-
-	console.log($('body').html());
+!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
 });
